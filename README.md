705e6c811793af84f6e692c077f95c9c2d2d03aa
Kevin O'Hara <kevinohara80@gmail.com>
2017-07-18 10:01:19 -0400

    finished access_token script

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 5. Put your certificate into the root of this project (einstein_platform.pem)
 6. Install node modules with `npm install`
 
-## Generating a Token
+## Generating an Access Token
 
 Once the setup steps have been completed, you can run the token script to 
 generate a signed JWT assertion that you will use to get an access token.
@@ -18,3 +18,13 @@ You must supply your username to the script.
 ```bash
 $ USER=kevinohara80@gmail.com node token.js
 ```
+
+When the script completes, you will have an access token. It will be output
+to you console as well as output to a json file (`./access_token.json`). You
+will use this token for making future requests.
+
+## Other Resources
+
+* If you use Postman, there is a Postman collection json config in this repo
+that you can import into Postman. You just need to set an `access_token` global
+variable to make all of the requests.
--- token.js
+++ token.js
@@ -1,13 +1,20 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
+const request = require('sync-request');
+const qs = require('querystring');
+
+if(!fs.existsSync('./einstein_platform.pem')) {
+  throw new Error('missing einstein_platform.pem file');
+}
+
 const cert = fs.readFileSync('./einstein_platform.pem', 'utf-8');
 
 const SFDC_USER = process.env.SFDC_USER;
 
 if (!SFDC_USER) throw new Error('You must supply a SFDC_USER environment variable');
 
-console.log('SFDC_USER: ' + SFDC_USER);
+console.log('obtaining token for user: ' + SFDC_USER);
 
 const payload = {
   "sub": SFDC_USER,
@@ -24,14 +31,27 @@ const options = {
 
 const token = jwt.sign(payload, cert, options);
 
-console.log('')
-console.log('TOKEN START >>>>>>')
-console.log(encodeURIComponent(token));
-console.log('<<<<<< TOKEN END')
-console.log('');
+const encodedToken = encodeURIComponent(token);
 
-const file = path.resolve(__dirname, './signed_token');
+const res = request('POST', 'https://api.einstein.ai/v2/oauth2/token', {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: qs.stringify({
+    'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+    'assertion': encodedToken
+  })
+});
 
-console.log('writing signed token: ' + file)
+const body = JSON.parse(res.body.toString());
 
-fs.writeFileSync(file, encodeURIComponent(token), { encoding: 'utf8' });
+if (!body.access_token) {
+  throw new Error('unable to obtain access token: ' + body.message);
+}
+
+const file = path.resolve(__dirname, 'access_token.json')
+
+console.log('access token: ' + body.access_token);
+console.log('writing access token to file: ' + file);
+
+fs.writeFileSync(file, JSON.stringify(body, null, '  '), { encoding: 'utf8' });
